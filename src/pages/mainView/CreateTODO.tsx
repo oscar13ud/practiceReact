d135341e524fee34243cff1ce6d318c61900e41b
Oscar Diaz <oscar13@udenar.edu.co>
2022-11-17 22:45:13 -0500

    Se agrega boton Crear

--- src/pages/mainView/CreateTODO.tsx
+++ src/pages/mainView/CreateTODO.tsx
@@ -3,12 +3,12 @@ import { Container } from "../../components/Container";
 export const CreateTODO = () => {
   return (
     <div className="h-100 d-flex justify-content-center align-items-center">
-      <Container height="h-75" width="w-75">
-        <div>
-            crear componentes para crear todos
-            pending
+      <Container height="h-50" width="w-75">
+        <div className="d-flex flex-column h-100 justify-content-around">
+          <h1 className="text-center pt-4">Crear TODO</h1>
+          <button className="button-3">Crear</button>
         </div>
-       </Container>
+      </Container>
     </div>
   );
 };
